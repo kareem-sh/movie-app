ba34bfc7fdc88f425dc0fae44b30e8a380b2f564
kareem-sh <karimshaheen467@gmail.com>
2025-07-09 22:52:21 +0300

    add assests

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="bg-emerald-500 text-red-500 h-10">
-      <h1 className="text-3xl">hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="/hero.png" alt="Hero Banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without The Hassle
+            </h1>
+          </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </div>
+      </div>
+    </main>
   );
 }
 
